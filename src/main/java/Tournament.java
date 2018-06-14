2f81019e066f37b16c316d2dabb6a214d7df0755
Luis Tejero <luisfaridta@gmail.com>
2018-06-14 18:35:29 +0100

    Refactoring and more testing

--- src/main/java/Tournament.java
+++ src/main/java/Tournament.java
@@ -20,17 +20,6 @@ public class Tournament implements PrizeAllocator{
         return  positions;
     }
 
-    public boolean checkIfHashContainsPlayer(HashMap<Integer, ArrayList<Participant>> positions, Participant player){
-        Integer key = 1;
-        for (int i = 0; i < positions.size(); i++ ){
-            if (positions.get(key).contains(player)){
-                return true;
-            }
-            key++;
-        }
-        return false;
-    }
-
     public HashMap<Integer, ArrayList<Participant>> createHashMapOfPositions(List<Participant> participants, Map<Integer, BigDecimal> prizes){
         List<Participant> list = sortParticipantsByScore(participants);
         HashMap<Integer, ArrayList<Participant>> positions = populateHashWithArraysForPositions(prizes);
@@ -55,52 +44,30 @@ public class Tournament implements PrizeAllocator{
         return positions;
     }
 
-    public BigDecimal getJackpot(Map<Integer, BigDecimal> prizes, Integer initialPosition, Integer finalPosition, Integer numberOfPlayers){
-        BigDecimal jackPot = new BigDecimal("0.00").setScale(2, BigDecimal.ROUND_FLOOR);
-        for (int i = 0; i < Math.min(prizes.size(), finalPosition); i++){
-            BigDecimal prize = prizes.get(initialPosition);
-            jackPot = jackPot.add(prize);
-            initialPosition++;
-        }
-        return jackPot.divide(new BigDecimal(numberOfPlayers), BigDecimal.ROUND_FLOOR);
-
-    }
-
-
     public void awardPrizes(List<Participant> participants, Map<Integer, BigDecimal> prizes) {
         //Build hash of participants placing in each position
         HashMap<Integer, ArrayList<Participant>> hashResults = this.createHashMapOfPositions(participants, prizes);
-
           //Initialise prize to start at
           int prizeKey = 1;
-
           //iterate through each participant result
           for(int i = 1; i <= hashResults.size(); i ++){
-
               //If noone came in a position then exit
               if(hashResults.get(i).size() == 0) break;
-
               // Get how many people placed in position i
               int numWinners = hashResults.get(i).size();
-
               // Initialise prize pool
               BigDecimal prizeValue = new BigDecimal("0.00").setScale(2, BigDecimal.ROUND_FLOOR);
-
               // start j at next prize
               int j = prizeKey;
-
               //get limit for j in the loop
               int limit = Math.min(prizeKey + numWinners, prizes.size() + 1);
               do{
                   // if we exceed bounds break
                   if(j > prizes.size()) break;
-
                   //get current prize and add it to the total prize pool
                   prizeValue = prizeValue.add(prizes.get(j));
                   j++;
               } while(j < limit);
-
-
               BigDecimal individualPrize = prizeValue.divide(new BigDecimal(numWinners), BigDecimal.ROUND_FLOOR);
               //assign individual prizes
               for(Participant participant : hashResults.get(i) ){
@@ -109,35 +76,5 @@ public class Tournament implements PrizeAllocator{
               //Update starting prize for next position
               prizeKey += numWinners;
           }
-//        Integer prizePosition = 1;
-//        Integer currentPosition = 1;
-//        for (int i = 0; i < prizes.size(); i++){
-//            Integer numberOfPlayersInPosition = hashResults.get(currentPosition).size();
-//            if (numberOfPlayersInPosition >= prizes.size()){
-//                BigDecimal jackPot = this.getJackpot(prizes, prizePosition, numberOfPlayersInPosition, numberOfPlayersInPosition);
-//                for (Participant player: hashResults.get(prizePosition)){
-//                    player.setPrize(jackPot);
-//                }
-//                break;
-////            } else if(numberOfPlayersInPosition == prizes.size()){
-////                BigDecimal jackPot = this.getJackpot(prizes, prizePosition, 1, numberOfPlayersInPosition);
-////                for (Participant player: hashResults.get(prizePosition)){
-////                    player.setPrize(jackPot);
-////                }
-////                break;
-//
-//            }else {
-//                BigDecimal jackPot = this.getJackpot(prizes, prizePosition, numberOfPlayersInPosition, numberOfPlayersInPosition);
-//                for (Participant player: hashResults.get(currentPosition)){
-//                    player.setPrize(jackPot);
-//                }
-//                prizePosition += numberOfPlayersInPosition;
-//                currentPosition++;
-//            }
-//            if (prizePosition > prizes.size()){
-//                break;
-//            }
-//        }
-//
     }
 }
--- src/test/java/TournamentTest.java
+++ src/test/java/TournamentTest.java
@@ -10,13 +10,14 @@ import java.util.Map;
 import static org.junit.Assert.assertEquals;
 
 public class TournamentTest {
-    Player player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12;
-    List<Participant> participantsList1, participantsList2, participantsList3, participantsList4, participantsList5;
+    Player player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13;
+    List<Participant> participantsList1, participantsList2, participantsList3, participantsList4, participantsList5, participantsList6;
     BigDecimal firstPrize;
     BigDecimal secondPrize;
     BigDecimal thirdPrize;
     BigDecimal fourthPrize;
-    Map<Integer, BigDecimal> prizes;
+    BigDecimal fifthPrize;
+    Map<Integer, BigDecimal> prizes, prizes1, prizes2;
     Tournament tournament1;
 
     @Before
@@ -33,16 +34,19 @@ public class TournamentTest {
         player10 = new Player("Carla", 10);
         player11 = new Player("Maria", 10);
         player12 = new Player("Mariano", 9);
+        player13 = new Player("Mariano", 2);
         firstPrize = new BigDecimal("50.00").setScale(2);
         secondPrize = new BigDecimal("20.00").setScale(2);
         thirdPrize = new BigDecimal("10.00").setScale(2);
         fourthPrize = new BigDecimal("5.00").setScale(2);
+        fifthPrize = new BigDecimal("2.00").setScale(2);
         tournament1 = new Tournament();
         participantsList1 = new ArrayList<>();
         participantsList2 = new ArrayList<>();
         participantsList3 = new ArrayList<>();
         participantsList4 = new ArrayList<>();
         participantsList5 = new ArrayList<>();
+        participantsList6 = new ArrayList<>();
         participantsList1.add(player1);
         participantsList1.add(player2);
         participantsList1.add(player3);
@@ -66,10 +70,32 @@ public class TournamentTest {
         participantsList5.add(player11);
         participantsList5.add(player8);
         participantsList5.add(player12);
+        participantsList5.add(player13);
+        participantsList6.add(player1);
+        participantsList6.add(player2);
+        participantsList6.add(player3);
+        participantsList6.add(player4);
+        participantsList6.add(player5);
+        participantsList6.add(player6);
+        participantsList6.add(player7);
+        participantsList6.add(player8);
+        participantsList6.add(player9);
+        participantsList6.add(player10);
         prizes = new HashMap<>();
+        prizes1 = new HashMap<>();
+        prizes2 = new HashMap<>();
         prizes.put(1, firstPrize);
         prizes.put(2, secondPrize);
         prizes.put(3, thirdPrize);
+        prizes1.put(1, firstPrize);
+        prizes1.put(2, secondPrize);
+        prizes1.put(3, thirdPrize);
+        prizes1.put(4, fourthPrize);
+        prizes2.put(1, firstPrize);
+        prizes2.put(2, secondPrize);
+        prizes2.put(3, thirdPrize);
+        prizes2.put(4, fourthPrize);
+        prizes2.put(5, fifthPrize);
     }
 
     @Test
@@ -89,16 +115,6 @@ public class TournamentTest {
         assertEquals("{1=[], 2=[], 3=[], 4=[], 5=[]}", tournament1.populateHashWithArraysForPositions(prizes).toString());
     }
 
-
-    @Test
-    public void canCheckIfPlayerIsInHash(){
-        HashMap<Integer, ArrayList<Participant>> positions = tournament1.populateHashWithArraysForPositions(prizes);
-        assertEquals(false, tournament1.checkIfHashContainsPlayer(positions, player1));
-        positions.get(2).add(player1);
-        assertEquals(true, tournament1.checkIfHashContainsPlayer(positions, player1));
-
-    }
-
     @Test
     public void canSetWinnersInHash() {
         HashMap<Integer, ArrayList<Participant>> hash = tournament1.createHashMapOfPositions(participantsList1, prizes);
@@ -113,19 +129,6 @@ public class TournamentTest {
         participantsList1.add(player5);
         HashMap<Integer, ArrayList<Participant>> hash2 = tournament1.createHashMapOfPositions(participantsList1, prizes);
         assertEquals("Daniel", hash2.get(3).get(0).getName());
-    }
-
-    @Test
-    public void canGetJackpot(){
-        assertEquals("50.00", tournament1.getJackpot(prizes, 1, 1, 1).toString());
-        assertEquals("35.00", tournament1.getJackpot(prizes, 1, 2, 2).toString());
-        assertEquals("20.00", tournament1.getJackpot(prizes, 1, 4, 4).toString());
-        assertEquals("16.00", tournament1.getJackpot(prizes, 1, 5, 5).toString());
-        assertEquals("8.88", tournament1.getJackpot(prizes, 1, 9, 9).toString());
-        assertEquals("20.00", tournament1.getJackpot(prizes, 2, 1, 1).toString());
-        assertEquals("15.00", tournament1.getJackpot(prizes, 2, 2, 2).toString());
-        assertEquals("5.00", tournament1.getJackpot(prizes, 3, 1, 2).toString());
-        assertEquals("3.33", tournament1.getJackpot(prizes, 3, 1, 3).toString());
     }
 
     @Test
@@ -154,7 +157,26 @@ public class TournamentTest {
         assertEquals("50.00", player8.getPrize().toString());
         assertEquals("15.00", player10.getPrize().toString());
         assertEquals("15.00", player11.getPrize().toString());
-        assertEquals("0.00", player12.getPrize().toString());
+        assertEquals("0.00",  player12.getPrize().toString());
+        assertEquals("0.00",  player13.getPrize().toString());
+        tournament1.awardPrizes(participantsList5, prizes1);
+        assertEquals("50.00", player8.getPrize().toString());
+        assertEquals("15.00", player10.getPrize().toString());
+        assertEquals("15.00", player11.getPrize().toString());
+        assertEquals("5.00",  player12.getPrize().toString());
+        assertEquals("0.00",  player13.getPrize().toString());
+        tournament1.awardPrizes(participantsList6, prizes2);
+        assertEquals("50.00", player9.getPrize().toString());
+        assertEquals("20.00", player8.getPrize().toString());
+        assertEquals("10.00", player7.getPrize().toString());
+        assertEquals("5.00", player6.getPrize().toString());
+        assertEquals("1.00", player4.getPrize().toString());
+        assertEquals("1.00", player2.getPrize().toString());
+        assertEquals("0.00", player1.getPrize().toString());
+        assertEquals("0.00", player10.getPrize().toString());
+        assertEquals("0.00", player3.getPrize().toString());
+        assertEquals("0.00", player5.getPrize().toString());
+
 
     }
 }
